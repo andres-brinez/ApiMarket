58a6dccae65f6e074903a7cc0d8ea7abc4a6c055
andres briñez <brinezlopez08@gmail.com>
2023-08-07 13:55:29 -0500

    Implementar la anotación @Repository

--- src/main/java/com/market/marketAPI/repositories/ProductoCrudRepository.java
+++ src/main/java/com/market/marketAPI/repositories/ProductoCrudRepository.java
@@ -2,11 +2,18 @@ package com.market.marketAPI.repositories;
 
 import com.market.marketAPI.models.Producto;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
     List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria); // Query Methods
     // query nativo
     // @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+
+
 }
