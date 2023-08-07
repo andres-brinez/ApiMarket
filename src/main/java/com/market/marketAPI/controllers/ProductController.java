86eb9d61d8d63428f96bacec5e8672ca9d3abd5a
andres briñez <brinezlopez08@gmail.com>
2023-08-07 14:52:27 -0500

    Enrutar aplicación

--- src/main/java/com/market/marketAPI/controllers/ProductController.java
+++ src/main/java/com/market/marketAPI/controllers/ProductController.java
@@ -1,9 +1,12 @@
 package com.market.marketAPI.controllers;
 
+import com.market.marketAPI.models.Producto;
 import com.market.marketAPI.services.ProductosServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/product")
@@ -12,5 +15,35 @@ public class ProductController {
     @Autowired // Conecta esta clase  con el servicio
     ProductosServices productosServices; // Se crea una instancia del servicio para poder usar los metodos de jpa
 
-    
+    @GetMapping("/all")
+    public List<Producto> getAllProductos(){
+        return productosServices.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Producto> getProductoById(@PathVariable("id") int id){
+        return productosServices.getProducto(id);
+    }
+
+    @GetMapping("/category/{id}")
+    public List<Producto> getByCategoria(@PathVariable("id") int idCategoria){
+        return productosServices.getByCategoria(idCategoria);
+    }
+
+    @PostMapping("/save")
+    public Producto save(@RequestBody Producto producto){
+        return productosServices.save(producto);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public boolean delete(@PathVariable("id") int idProducto){
+        return productosServices.delete(idProducto);
+    }
+
+    @GetMapping("/escasos/{cantidad}")
+    public Optional<List<Producto>> getEscasos(@PathVariable("cantidad") int cantidad){
+        return productosServices.getEscasos(cantidad);
+    }
+
+
 }
