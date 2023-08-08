de95391c8350bfba25410dc40fbd35a3d0093d46
andres briñez <brinezlopez08@gmail.com>
2023-08-07 19:27:23 -0500

    Intentar ocumentar nuestra API con Swagger

--- src/main/java/com/market/marketAPI/MarketApiApplication.java
+++ src/main/java/com/market/marketAPI/MarketApiApplication.java
@@ -2,6 +2,13 @@ package com.market.marketAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
 public class MarketApiApplication {
@@ -9,5 +16,16 @@ public class MarketApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MarketApiApplication.class, args);
 	}
+
+
+		@Bean
+
+		public Docket api() {
+			return new Docket(DocumentationType.SWAGGER_2)
+					.select()
+					.apis(RequestHandlerSelectors.basePackage("com.example.myapp"))
+					.paths(PathSelectors.any())
+					.build();
+		}
 
 }
